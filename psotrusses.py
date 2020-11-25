fb4c8c9cdc0be9ed1df16d28e16a47691cf4dbfc
Leonardo-Maciel <leo.gomes.maciel@gmail.com>
2020-11-25 15:50:32 -0300

    plota graficamente a treliça-psotrusses-

--- psotrusses.py
+++ psotrusses.py
@@ -1,1 +1,22 @@
+class Createtruss:
 
+    def __init__(self, x, y, connect):
+        self.x = x
+        self.y = y
+        self.connect = connect
+        self.point = []
+        self.x1 = []
+        self.y1 = []
+
+    def plot(self, plot='no'):
+        if plot == 'yes':
+            for xi, yi in zip(self.x, self.y):
+                self.point.append([xi, yi])
+                plt.scatter(xi, yi, color='grey')
+            for i in range(len(self.connect)):
+                self.x1.append(self.point[self.connect[i][0]][0])
+                self.x1.append(self.point[self.connect[i][1]][0])
+                self.y1.append(self.point[self.connect[i][0]][1])
+                self.y1.append(self.point[self.connect[i][1]][1])
+            plt.plot(self.x1, self.y1)
+            plt.show()
